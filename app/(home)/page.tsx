0f1074675f4bc7eadf2696dcf46bffb239af95ec
Harish Chandran <harish3304.work@gmail.com>
2025-10-19 23:57:57 +0530

    docs: update home page

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -8,11 +8,18 @@ export default function HomePage() {
       <Image
         src={banner}
         alt="better-env banner"
-        className="mx-auto mb-10 h-auto w-full max-w-6xl rounded-xl shadow-lg"
+        className="mx-auto mb-10 h-auto w-full max-w-2xl rounded-xl shadow-lg"
         priority
       />
+      <h1 className="mx-auto mb-6 max-w-3xl text-balance text-4xl font-bold text-fd-foreground md:text-4xl">
+        A better way to manage secrets
+        <br />
+        because screw .env
+      </h1>
       <p className="mx-auto max-w-2xl text-balance text-lg text-fd-muted-foreground md:text-xl">
-        Encrypted secrets, zero plaintext, instant runtime loading.{' '}
+        Encrypted Secrets • Zero Plaintext • Instant Runtime Loading • 100% Commit Safe
+        <br />
+        <br />
         <Link
           href="/docs"
           className="text-fd-foreground font-semibold underline"
